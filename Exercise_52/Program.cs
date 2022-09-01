9c1566176ba1fe3d02d2679426d83190ab2dd60d
Artem Kandakov <Temofey1993@gmail.com>
2022-09-01 20:44:33 +0500

    Задача №52

---
+++ Exercise_52/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Console.WriteLine("Введите высоту матрицы = ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите длинну длинну = ");
+int n = int.Parse(Console.ReadLine());
+int[ , ] array = new int[m,n];
+void FillArray(int [ , ] matr )
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+void PrintArray(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+                Console.Write(matr[i,j] + "|");
+        }
+    Console.WriteLine();
+    }
+    
+}
+double result = 0;
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.Write("Среднее арифметическое каждого толбца = ");
+   for(int j = 0; j < array.GetLength(0); j++)
+    {
+        result = 0;
+        for(int i = 0; i < array.GetLength(1); i++)
+        {
+        result = result + array[i,j];
+        }
+        Console.Write(result/m+"|");
+    }
